8bca0f2a07e86740134d6e7ce96a1add1af97671
ishita1505 <agrawalishita15@gmail.com>
2023-06-26 13:55:07 +0530

    Image upload feature added

--- realist/server/server.js
+++ realist/server/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { DATABASE } from "./config.js";
 import authRoutes from "./routes/auth.js";
-
+import adRoutes from "./routes/ad.js"
 const app = express();
 
 // db
@@ -15,10 +15,11 @@ mongoose
   .catch((err) => console.log(err));
 
 // middlewares
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(morgan("dev"));
 app.use(cors());
 // routes middleware
 app.use("/api", authRoutes);
+app.use("/api",adRoutes);
 
 app.listen(8000, () => console.log("server_running_on_port_8000"));
